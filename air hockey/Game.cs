4ca51c5f0bf9edfb5a0297b2167ecfdc338c08a2
GmaB-a <vladik11012006@gmail.com>
2022-05-25 20:41:13 +0300

    changes

--- air hockey/Game.cs
+++ air hockey/Game.cs
@@ -74,12 +74,12 @@ namespace air_hockey
             if (Keyboard.IsKeyPressed(Keyboard.Key.W)) player1Position.Y -= lineSpeed;
             if (Keyboard.IsKeyPressed(Keyboard.Key.S)) player1Position.Y += lineSpeed;
             if (player1Position.Y < line1.Size.Y) player1Position.Y = line1.Size.Y;
-            if (player1Position.Y > window.Size.Y - line1.Size.Y) player1Position.Y = window.Size.Y - line1.Size.Y;
+            if (player1Position.Y + line1.Size.Y > window.Size.Y) player1Position.Y = window.Size.Y - line1.Size.Y;
 
             if (Keyboard.IsKeyPressed(Keyboard.Key.Up)) player2Position.Y -= lineSpeed;
             if (Keyboard.IsKeyPressed(Keyboard.Key.Down)) player2Position.Y += lineSpeed;
-            if (player2Position.Y < line2.Size.Y) player2Position.Y = line2.Size.Y;
-            else if (player2Position.Y > window.Size.Y - line2.Size.Y) player2Position.Y = window.Size.Y - line2.Size.Y;
+            if (player2Position.Y < 0) player2Position.Y = line2.Size.Y;
+            if (player2Position.Y + line2.Size.Y > window.Size.Y) player2Position.Y = window.Size.Y - line2.Size.Y;
         }
 
         void DrawScores()
@@ -102,13 +102,12 @@ namespace air_hockey
             if (!isBallMoving)
             {
                 while (ballMoveDirection == new SFML.System.Vector2f(0,0)) 
-                    ballMoveDirection = new SFML.System.Vector2f((float)rnd.NextDouble() * rnd.Next(-1, 1), (float)rnd.NextDouble() * rnd.Next(-1, 1));
+                    ballMoveDirection = new SFML.System.Vector2f((float)rnd.NextDouble() * rnd.Next(-1, 2), (float)rnd.NextDouble() * rnd.Next(-1, 2));
                 isBallMoving = true;
             }
             ballPosition += ballMoveDirection * ballSpeed;
             CheckIfSomebodyGotScore(ball);
-            CheckIfShouldChangeDirection(ball);
-            CheckIfHitPlayerLine(ball);
+            CheckIfShouldChangeDirection(ball);;
             window.Draw(ball);
         }
 
@@ -131,17 +130,18 @@ namespace air_hockey
         {
             if (ball.Position.Y > window.Size.Y - ball.Radius && ballMoveDirection.Y > 0) ballMoveDirection.Y *= -1f;
             else if (ball.Position.Y < ball.Radius && ballMoveDirection.Y < 0) ballMoveDirection.Y *= -1f;
+            CheckIfHitPlayerLine(ball);
         }
         void CheckIfHitPlayerLine(CircleShape ball)
         {
-            if ((ball.Position.Y > line1.Position.Y - line1.Size.Y) && (ball.Position.Y < line1.Position.Y + line1.Size.Y))
+            if ((ball.Position.Y > line1.Position.Y) && (ball.Position.Y < line1.Position.Y + line1.Size.Y))
             {
-                if ((line1.Position.X >= ball.Position.X + ball.Radius) && (line1.Position.X <= ball.Position.X)) ballMoveDirection *= -1f;
+               if (line1.Position.X >= ball.Position.X + ball.Radius) ballMoveDirection.X *= -1f;
             }
 
-            if ((ball.Position.Y > line2.Position.Y - line1.Size.Y) && (ball.Position.Y < line2.Position.Y + line1.Size.Y))
+            if ((ball.Position.Y > line2.Position.Y) && (ball.Position.Y < line2.Position.Y + line2.Size.Y))
             {
-                if ((line2.Position.X <= ball.Position.X + ball.Radius) && (line2.Position.X >= ball.Position.X)) ballMoveDirection *= -1f;
+               if (line2.Position.X >= ball.Position.X + ball.Radius) ballMoveDirection.X *= -1f;
             }
         }
     }
